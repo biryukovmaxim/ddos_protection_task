c26d71093beedd6ec6cba9dbb02bbd43d527e74f
maksim <maksim@blockchaincmdt.io>
2023-01-30 14:05:09 +0400

    dockerfile for client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -4,19 +4,30 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"os"
+	"strconv"
 	"time"
 )
 
+var (
+	outgoingPort = os.Getenv("OUT_PORT")
+	destination  = os.Getenv("DEST")
+)
+
 func main() {
+	port, err := strconv.Atoi(outgoingPort)
+	if err != nil {
+		panic(err)
+	}
 	dialer := net.Dialer{
 		Timeout: 3 * time.Second,
 		LocalAddr: &net.TCPAddr{
 			IP:   nil,
-			Port: 43720,
+			Port: port,
 			Zone: "",
 		},
 	}
-	conn, err := dialer.Dial("tcp", "localhost:5051")
+	conn, err := dialer.Dial("tcp", destination)
 	if err != nil {
 		fmt.Println(err)
 		return
