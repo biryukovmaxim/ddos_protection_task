37e65a366176346172d6cbf5a24c2e4a33a77121
maksim <maksim@blockchaincmdt.io>
2023-01-31 19:45:29 +0400

    tmp commit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
-	"crypto"
+	_ "crypto"
 	_ "crypto/sha256"
 	"fmt"
 	"net"
 	"os"
-	"sync"
+	_ "sync"
 
-	"ddos_protection_task/internal/verifier"
-	"ddos_protection_task/pkg/challenge"
+	_ "ddos_protection_task/internal/verifier"
+	_ "ddos_protection_task/pkg/challenge"
+
+	goebpf "github.com/cilium/ebpf"
 
 	log "github.com/sirupsen/logrus"
+	"golang.org/x/sys/unix"
 )
 
 var (
@@ -32,17 +35,18 @@ func main() {
 	//	log.WithError(err).Fatal("getting net interfaces")
 	//}
 	//log.Debugf("%+v", inerfaces)
-	//if err := unix.Setrlimit(unix.RLIMIT_MEMLOCK, &unix.Rlimit{
-	//	Cur: unix.RLIM_INFINITY,
-	//	Max: unix.RLIM_INFINITY,
-	//}); err != nil {
-	//	log.WithError(err).Error("set rlimit")
-	//}
-	//bpf := goebpf.NewDefaultEbpfSystem()
-	//if err := bpf.LoadElf(pathToElf); err != nil {
-	//	log.WithError(err).Error("load elf")
-	//}
-	//xdp := bpf.GetProgramByName("outer_firewall")
+	if err := unix.Setrlimit(unix.RLIMIT_MEMLOCK, &unix.Rlimit{
+		Cur: unix.RLIM_INFINITY,
+		Max: unix.RLIM_INFINITY,
+	}); err != nil {
+		log.WithError(err).Error("set rlimit")
+	}
+	collection, err := goebpf.LoadCollection(pathToElf)
+	if err != nil {
+		panic(err)
+	}
+	collection.Close()
+	//xdp := bpf.XDPProgram("outer_firewall")
 	//if err := xdp.Load(); err != nil {
 	//	log.Fatalf("xdp.Load(): %v", err)
 	//}
@@ -54,59 +58,59 @@ func main() {
 	//}
 	//defer xdp.Detach()
 
-	listener, err := net.Listen("tcp", "localhost:5051")
-	if err != nil {
-		log.WithError(err).Fatalf("start listeing")
-	}
-	defer listener.Close()
-
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-	log.Info("start listening incoming tcp conns")
-	go func() {
-		defer wg.Done()
-		for {
-			conn, err := listener.Accept()
-			if err != nil {
-				log.WithError(err).Warn("accepting incoming conn")
-				continue
-			}
-			go handleRequest(conn)
-		}
-	}()
-
-	vf := verifier.NewService(challenge.ChallengeSize, challenge.Difficulty, crypto.SHA256, MockMap{map[interface{}]interface{}{}})
-	server := challenge.NewServer(vf)
-	udpServer, err := net.ListenPacket("udp", ":1053")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer udpServer.Close()
-	log.Info("start listening incoming udp frames")
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for {
-			buf := make([]byte, 1024)
-			_, addr, err := udpServer.ReadFrom(buf)
-			if err != nil {
-				continue
-			}
-			go func(buf []byte, addr net.Addr) {
-				resp, err := server.ProcessRequest(addr.(*net.UDPAddr), buf)
-				if err != nil {
-					log.WithError(err).Error("processing udp request")
-					return
-				}
-				_, err = udpServer.WriteTo(resp, addr)
-				if err != nil {
-					log.WithError(err).Error("sending udp response")
-				}
-
-			}(buf, addr)
-		}
-	}()
-	wg.Wait()
+	//listener, err := net.Listen("tcp", "localhost:5051")
+	//if err != nil {
+	//	log.WithError(err).Fatalf("start listeing")
+	//}
+	//defer listener.Close()
+	//
+	//wg := &sync.WaitGroup{}
+	//wg.Add(1)
+	//log.Info("start listening incoming tcp conns")
+	//go func() {
+	//	defer wg.Done()
+	//	for {
+	//		conn, err := listener.Accept()
+	//		if err != nil {
+	//			log.WithError(err).Warn("accepting incoming conn")
+	//			continue
+	//		}
+	//		go handleRequest(conn)
+	//	}
+	//}()
+	//
+	//vf := verifier.NewService(challenge.ChallengeSize, challenge.Difficulty, crypto.SHA256, MockMap{map[interface{}]interface{}{}})
+	//server := challenge.NewServer(vf)
+	//udpServer, err := net.ListenPacket("udp", ":1053")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//defer udpServer.Close()
+	//log.Info("start listening incoming udp frames")
+	//wg.Add(1)
+	//go func() {
+	//	defer wg.Done()
+	//	for {
+	//		buf := make([]byte, 1024)
+	//		_, addr, err := udpServer.ReadFrom(buf)
+	//		if err != nil {
+	//			continue
+	//		}
+	//		go func(buf []byte, addr net.Addr) {
+	//			resp, err := server.ProcessRequest(addr.(*net.UDPAddr), buf)
+	//			if err != nil {
+	//				log.WithError(err).Error("processing udp request")
+	//				return
+	//			}
+	//			_, err = udpServer.WriteTo(resp, addr)
+	//			if err != nil {
+	//				log.WithError(err).Error("sending udp response")
+	//			}
+	//
+	//		}(buf, addr)
+	//	}
+	//}()
+	//wg.Wait()
 }
 
 func handleRequest(conn net.Conn) {
--- go.mod
+++ go.mod
@@ -3,13 +3,10 @@ module ddos_protection_task
 go 1.19
 
 require (
-	github.com/dropbox/goebpf v0.0.0-20221212193212-e568275f8431
+	github.com/cilium/ebpf v0.10.0
 	github.com/orcaman/concurrent-map/v2 v2.0.1
 	github.com/sirupsen/logrus v1.9.0
 	golang.org/x/sys v0.2.0
 )
 
-require (
-	github.com/vishvananda/netlink v1.1.1-0.20200218174631-5f2fc868c2d0 // indirect
-	github.com/vishvananda/netns v0.0.0-20200520041808-52d707b772fe // indirect
-)
+require github.com/stretchr/testify v1.8.1 // indirect
